dfcde4c7ede4d3b80b83ecf4c34b02449aa8cee5
Szabolcs Bokonyi <szabcsee@gmail.com>
2014-10-03 01:26:04 +0200

    Proposals and proposal implemented

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -1,32 +1,68 @@
 var lufibooksApp = angular.module('lufibooks', ['ngRoute']).config(function($routeProvider){
 	$routeProvider.when('/main', {
 			templateUrl: 'partials/main.html',
-			controller: 'newBooksCtrl'
+			controller: 'MainCtrl'
 		}).when('/books', {
 			templateUrl: 'partials/books.html',
-			controller: 'booksCtrl'
+			controller: 'BooksCtrl'
 		}).when('/proposals', {
 			templateUrl: 'partials/proposals.html',
-			controller: 'proposalsCtrl'
+			controller: 'ProposalsCtrl'
 		}).when('/proposals/:proposal_id', {
-			templateUrl: 'partials/proposal-detail.html',
-			controller: 'newProposalCtrl'
+			templateUrl: 'partials/proposal.html',
+			controller: 'NewProposalCtrl'
 		}).when('/login', {
 			templateUrl: 'partials/login.html',
-			controller: 'loginCtrl'
+			controller: 'LoginCtrl'
 		}).when('/borrowed:books_id', {
 			templateUrl: 'partials/borrowed.html',
-			controller: 'borrowedCtrl'
+			controller: 'BorrowedCtrl'
 		}).otherwise({
 			redirectTo: '/main'
 		});
 });
 
-function proposalsCtrl($scope, $http) {
+lufibooksApp.controller('ProposalsCtrl', function($scope, $http, $location) {
 	$http.get('/proposals').success(function(data) {
+		$scope.proposals = data.proposal;
+	});
+
+	$scope.VoteUp = function(clickEvent, key) {
+		var proposalData = {"proposal-key" : key}
+		$http.post('/vote-ups', proposalData )
+	};
+	$scope.submitIsbn = function(clickEvent, isbnNumber) {
+		var isbnData = {"proposal": {"isbn" : isbnNumber}};
+		 $http.post('/proposals', isbnData ).success(function(response){
+			console.log(response.proposal.key);
+			$location.url('/proposals/' + response.proposal.key);
+		});
+	};
+});
+
+function NewBooksCtrl($scope, $http) {
+	$http.get('/books').success(function(data) {
 		$scope.proposals = data;
 	});
 }
 
+function MainCtrl($scope, $http) {
+	$http.get('/books').success(function(data) {
+		$scope.proposals = data;
+	});
+}
 
+lufibooksApp.controller('NewProposalCtrl', function($scope, $http, $routeParams, $location) {
+	$http.get('/proposals/' + $routeParams.proposal_id).success(function(data) {
+		$scope.proposal = data.proposal;
+	});
+});
+
+function LoginCtrl($scope, $http) {
+
+}
+
+function BorrowedCtrl($scope, $http) {
+
+}
 
