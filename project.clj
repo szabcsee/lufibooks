c634a50639d36d71cd57d74176d423ee81d2be98
U292197 <attila-zsolt.piros@lhsystems.com>
2015-04-24 20:38:13 +0200

    add yesql
    ----------------------
    branch [[master]]

--- project.clj
+++ project.clj
@@ -9,16 +9,24 @@
                  [com.cemerick/friend "0.2.1"]
                  [ring-server "0.3.1"]
                  [ring/ring-json "0.3.1"]
-                 [hyperion/hyperion-api "3.7.1"]
                  [org.clojars.freeagent/clj-amazon "0.2.2-SNAPSHOT"]
-                 ]
-  :plugins [[lein-ring "0.8.12"]]
+				 [hyperion/hyperion-api "3.7.1"]
+				 [yesql "0.4.0"]
+                 [org.apache.derby/derby "10.11.1.1"]
+                 [log4j "1.2.15" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]]
+  :plugins [[lein-ring "0.8.12"]
+            [cider/cider-nrepl "0.7.0"]]
   :ring {:handler lufibooks.handler/app
          :init lufibooks.handler/init
          :destroy lufibooks.handler/destroy
          :open-browser? false
          :stacktraces? true
          :auto-reload? true}
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
